fe479ca08c4edada75c4ad932d79e06d6655d06b
Cédric Mujynya <cedric.mujynya@edu.hedr.ch>
2021-12-14 22:49:55 +0100

    Kubernetes WIP - all

--- Kubernetes/cassandra/init.sh
+++ Kubernetes/cassandra/init.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-echo "============================"
-sleep 1
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.5.3/cert-manager.yaml
 COUNT=$(expr $(kubectl -n cert-manager get pods | wc -l) - 2)
@@ -11,7 +10,7 @@ do
     kubectl -n cert-manager wait --for=condition=ready pod $NAME
 done
 echo "============================"
-sleep 2
+sleep 5
 
 
 kubectl apply -k github.com/k8ssandra/cass-operator/config/deployments/default
@@ -25,13 +24,13 @@ echo "============================"
 
 
 SCNAME=$(kubectl get sc -o "jsonpath={.items[0].metadata.name}")
-cp cassdc1_orig.yml cassdc1.yml
-sed -i "s/XXXXXX/$SCNAME/" cassdc1.yml
-kubectl -n cass-operator apply -f cassdc1.yml
+cp "$SCRIPTPATH/cassdc1_orig.yml" "$SCRIPTPATH/cassdc1.yml"
+sed -i "s/XXXXXX/$SCNAME/" "$SCRIPTPATH/cassdc1.yml"
+kubectl -n cass-operator apply -f "$SCRIPTPATH/cassdc1.yml"
 echo "============================"
 
 export STATUS="XXXXX"
-while [ $STATUS != "Ready" ]
+while [[ $STATUS != "Ready" ]]
 do
     sleep 5
     STATUS=$(kubectl -n cass-operator get cassdc/dc1 -o "jsonpath={.status.cassandraOperatorProgress}")
@@ -50,16 +49,16 @@ echo "============================"
 export OPERATION="CREATE SCHEMA bbdata2 WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 3 };"
 kubectl -n cass-operator exec -ti cluster1-dc1-default-sts-0 -c cassandra -- sh -c "cqlsh -u '$CASS_USER' -p '$CASS_PASS' -e \"$OPERATION\""
 echo "============================"
-export OPERATION="CREATE TABLE bbdata2.raw_values (object_id int, timestamp timestamp, comment text, month text, value text, PRIMARY KEY ((object_id, month), timestamp)) WITH CLUSTERING ORDER BY (timestamp ASC);"
+OPERATION="CREATE TABLE bbdata2.raw_values (object_id int, timestamp timestamp, comment text, month text, value text, PRIMARY KEY ((object_id, month), timestamp)) WITH CLUSTERING ORDER BY (timestamp ASC);"
 kubectl -n cass-operator exec -ti cluster1-dc1-default-sts-0 -c cassandra -- sh -c "cqlsh -u '$CASS_USER' -p '$CASS_PASS' -e \"$OPERATION\""
 echo "============================"
-export OPERATION="CREATE TABLE bbdata2.aggregations (minutes int, object_id int, date text, timestamp timestamp, last float, last_ts bigint, min float, max float, sum float, mean float, count int, k float, k_sum float, k_sum_2 float, std float, PRIMARY KEY ((minutes, object_id, date), timestamp)) WITH CLUSTERING ORDER BY (timestamp DESC);"
+OPERATION="CREATE TABLE bbdata2.aggregations (minutes int, object_id int, date text, timestamp timestamp, last float, last_ts bigint, min float, max float, sum float, mean float, count int, k float, k_sum float, k_sum_2 float, std float, PRIMARY KEY ((minutes, object_id, date), timestamp)) WITH CLUSTERING ORDER BY (timestamp DESC);"
 kubectl -n cass-operator exec -ti cluster1-dc1-default-sts-0 -c cassandra -- sh -c "cqlsh -u '$CASS_USER' -p '$CASS_PASS' -e \"$OPERATION\""
 echo "============================"
-export OPERATION="CREATE TABLE bbdata2.objects_stats_counter (object_id int, n_reads counter, n_values counter, PRIMARY KEY (object_id));"
+OPERATION="CREATE TABLE bbdata2.objects_stats_counter (object_id int, n_reads counter, n_values counter, PRIMARY KEY (object_id));"
 kubectl -n cass-operator exec -ti cluster1-dc1-default-sts-0 -c cassandra -- sh -c "cqlsh -u '$CASS_USER' -p '$CASS_PASS' -e \"$OPERATION\""
 echo "============================"
-export OPERATION="CREATE TABLE bbdata2.objects_stats (object_id int, avg_sample_period float, last_ts timestamp, PRIMARY KEY (object_id));"
+OPERATION="CREATE TABLE bbdata2.objects_stats (object_id int, avg_sample_period float, last_ts timestamp, PRIMARY KEY (object_id));"
 kubectl -n cass-operator exec -ti cluster1-dc1-default-sts-0 -c cassandra -- sh -c "cqlsh -u '$CASS_USER' -p '$CASS_PASS' -e \"$OPERATION\""
 echo "============================"
 
--- Kubernetes/init.sh
+++ Kubernetes/init.sh
@@ -3,9 +3,43 @@
 # Prerequisites:    - jq:       sudo apt install jq
 #                   - k3d:      curl -s https://raw.githubusercontent.com/rancher/k3d/main/install.sh | bash
 
-k3d cluster create myCluster -a 3
+if ! command -v jq &> /dev/null
+then
+    echo "jq could not be found !"
+    read -r -p "Would you like to install it ? [Y/n]" response
+    response=${response,,} # tolower
+    if [[ $response =~ ^(yes|y| ) ]] || [[ -z $response ]]; then
+        sudo apt install jq
+    else
+        exit
+    fi
+fi
+
+if ! command -v k3d &> /dev/null
+then
+    echo "k3d could not be found !"
+    read -r -p "Would you like to install it ? [Y/n]" response
+    response=${response,,} # tolower
+    if [[ $response =~ ^(yes|y| ) ]] || [[ -z $response ]]; then
+        curl -s https://raw.githubusercontent.com/rancher/k3d/main/install.sh | bash
+    else
+        exit
+    fi
+fi
+
+k3d cluster create myCluster -p "8080:30080@agent:0" -a 3
 
 echo "===================       STARTING CASSANDRA       ==================="
 ./cassandra/init.sh
 echo "=================== CASSANDRA DONE, STARTING KAFKA ==================="
 ./kafka/init.sh
+echo "===================   KAFKA DONE, STARTING MYSQL   ==================="
+kubectl apply -f mysql-deployment.yml
+NAME=$(kubectl get pods -l app=mysql -o "jsonpath={.items[0].metadata.name}")
+kubectl wait --for=condition=ready pod $NAME
+echo "===================   MYSQL DONE, STARTING SPRING   ==================="
+kubectl apply -f spring-deployment.yml
+NAME=$(kubectl get pods -l app=spring -o "jsonpath={.items[0].metadata.name}")
+kubectl wait --for=condition=ready pod $NAME
+kubectl apply -f spring-service.yml
+echo "===================      SPRING DONE, ALL DONE      ==================="
